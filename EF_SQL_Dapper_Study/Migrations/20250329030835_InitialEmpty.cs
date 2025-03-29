74fd2f72b4cae765ccf63782599338767c66226a
Taras Karpuk <taras.karpuk@gmail.com>
2025-03-28 21:16:11 -0600

    add empty migration after scaffolding for existing DB (Database-First)

---
+++ EF_SQL_Dapper_Study/Migrations/20250329030835_InitialEmpty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_SQL_Dapper_Study.Migrations
+{
+    /// <inheritdoc />
+    /// Empty migration because of using scaffolding of existing DB
+    public partial class InitialEmpty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
