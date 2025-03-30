b1879036a38b421d6a44343730967afd9e58b3b4
Taras Karpuk <taras.karpuk@gmail.com>
2025-03-29 18:20:43 -0600

    create VIEW_DepartmentEmployees in DB

---
+++ EF_SQL_Dapper_Study/Migrations/20250330001549_Create_View.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_SQL_Dapper_Study.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_View : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE VIEW march.View_DepartmentEmployees AS
+	                               SELECT d.Id, d.Name, e.FullName AS EmployeeFullName, e.Email, e.HireDate
+	                               FROM march.Departments d
+	                               JOIN march.Employees e ON e.DepartmentId = d.Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW IF EXISTS march.View_DepartmentEmployees");
+        }
+    }
+}
